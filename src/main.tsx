46751f817e5659654770eedb28bbb170351c39b4
Aung The Ko <aung.tk@mit.com.mm>
2026-02-22 18:17:53 +0630

    SCRUM-23 : Setting  Up Mobox for state management and Input show/hide password in login form and Log Out function

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { StoreContext } from './components/store/StoreContext.ts'
+import { rootStore } from './components/store/rootStore.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <StoreContext.Provider value={rootStore}>
       <App />
-  </StrictMode>,
+  </StoreContext.Provider>
 )
