4df2d60cb0c6ea67d92196ae9182ae91ec1afaa6
Hnin Aein Lwin <hninaein2017@gmail.com>
2026-03-03 21:42:45 +0700

    SCRUM26-Upload Blog Posts and Blog List View [frontend]

--- src/components/dashboard/StudentDashboard.tsx
+++ src/components/dashboard/StudentDashboard.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import DashboardLayout from "./layout/DashboardLayout";
+import BlogList from "../blog/BlogList";
+import NewBlog from '../blog/NewBlog';
 
 const IconDashboard = ({ className }: any) => (
     <svg
@@ -20,28 +22,50 @@ const StudentDashboard = () => {
     const [activeTab, setActiveTab] = useState("dashboard");
 
     const menu = [
-        { key: "dashboard", label: "Dashboard", icon: IconDashboard }
+        { key: "dashboard", label: "Dashboard", icon: IconDashboard },
     ];
+
+    const [showBlogs, setShowBlogs] = useState(false);
+    const [showNewBlog, setShowNewBlog] = useState(false);
 
     const renderContent = () => {
         switch (activeTab) {
             case "dashboard":
-                return <div></div>;
+            return showBlogs ? (
+                <BlogList onBack={() => setShowBlogs(false)} />
+            ) : (
+                <div className="p-6">
+                    <button
+                        onClick={() => setShowBlogs(true)}
+                        className="px-4 py-2 text-sm font-semibold bg-gradient-to-tr from-orange-500 to-rose-500 text-white rounded-lg hover:opacity-90 transition"
+                    >
+                        See More Blogs
+                    </button>
+                </div>
+            );
             default:
                 return;
         }
     };
 
     return (
+        <>
         <DashboardLayout
             menu={menu}
             activeTab={activeTab}
             onTabChange={setActiveTab}
             name="Student Dashboard"
+            onBlogClick={() => setShowNewBlog(true)}  
         >
             {renderContent()}
         </DashboardLayout>
+        
+        {showNewBlog && (
+            <NewBlog show={showNewBlog} onClose={() => setShowNewBlog(false)} />
+        )}
+        </>
     );
+
 };
 
 export default StudentDashboard;
